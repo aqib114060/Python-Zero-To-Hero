2a7828c2fee584d55b9bf82d079b2e16b20e2a3d
Hafiz Muhammad Aqib <aqib114060@gmail.com>
2026-07-23 15:33:19 +0500

    Add files via upload

---
+++ 1_calculator.py
@@ -0,0 +1,12 @@
+value_1 = input("please enter your first number: ")
+value_2 = input("please enter your second number: ")
+
+
+value_1 = int(value_1)
+value_2 = int(value_2)
+
+sum = value_1 + value_2
+subtract = value_1 - value_2
+print(subtract)
+print("sum of given values is: ", sum)
+
